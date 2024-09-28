166a6d367c7dafb9f810e87489c6cd778f5f6856
MJ <tejeromaryjessica27@gmail.com>
2024-09-29 05:26:39 +0800

    clearing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginForm from './Components/Auth/LoginForm';
+import RegistrationForm from './Components/Auth/RegistrationForm';
+import StudentHomePage from './Components/Student/HomePage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>
-          QueueIt
-        </h1>
-         
-      </header>
-    </div>
-  );
+    return (
+
+        <Router>
+            <Routes>
+                <Route path="/login" element={<LoginForm />} />
+                <Route path="/register" element={<RegistrationForm />} />
+                <Route path="/studenthomepage" element={<StudentHomePage />} />
+                <Route path="/" element={
+                  <RegistrationForm/>} />
+                <Route path="*" element={<h1>404 Not Found</h1>} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
